daaeb8b4a7a23adf82afceb31985bb80a7317fc2
joe10994 <joe10994@gmail.com>
2020-01-06 12:29:44 -0500

    Create game logic function checkWin
    Game logic added to check the current board against all win conditions.

--- assets/scripts/game/logic.js
+++ assets/scripts/game/logic.js
@@ -1,14 +1,30 @@
 let currentTurn = 'x'
+const occupiedSpots = new Array(9)
 
 const onAttemptTurn = function (event) {
-  if (!($(`#box${this.id.slice(3)}`).hasClass('clicked'))) {
-    $(`#box${this.id.slice(3)}`).text(currentTurn).addClass('clicked')
+  if (!($(`#${this.id}`).hasClass('clicked'))) { // if the spot on the board does not have the class clicked ,  add the move to the board and add the class to the spot
+    $(`#${this.id}`).text(currentTurn).addClass('clicked')
+    occupiedSpots[this.id.slice(3)] = currentTurn // add the move to the occupiedSpots array
+    if (checkWin()) {
+      console.log('winnah')
+    }
     currentTurn === 'x' ? currentTurn = 'o' : currentTurn = 'x'
   }
 }
 
-
-
+const checkWin = function () {
+  let won = false
+  const winConditions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [3, 4, 2]]// diagonal
+  for (const condition of winConditions) {
+    if (occupiedSpots[condition[0]] === currentTurn && occupiedSpots[condition[1]] === currentTurn && occupiedSpots[condition[2]] === currentTurn) {
+      won = true
+    }
+  }
+  return won
+}
 module.exports = {
   onAttemptTurn
 }
