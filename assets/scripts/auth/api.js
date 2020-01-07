84c51f1188217707bc7c38ffe31dd25aff76c0f1
joe10994 <joe10994@gmail.com>
2020-01-07 00:01:10 -0500

    Add ui and upi logic for sign in/up
    Added event handlers and basic user feedback as well as the ability to
    sign in and up. Created changeView function to swap between logged in
    and logged out states, may need to be refactored later based on future
    needs. Currently game board is hidden until sign in, then you may play.

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = (data) => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
+
+const signIn = (data) => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+
+
+module.exports = {
+  signUp,
+  signIn
+}
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,53 @@
 'use strict'
+const store = require('../store')
+
+const signUpSuccess = function (response) {
+  $('#messages').text('Successfully Signed Up!')
+  console.log(response)
+}
+
+const signUpFailure = function (error) {
+  $('#sign-in').trigger('reset')
+  $('#sign-up').trigger('reset')
+  $('#messages').text('Failed to Sign Up!')
+  console.log(error)
+}
+
+const signInSuccess = function (response) {
+  $('#messages').text('Successfully Signed In!')
+  store.user = response.user
+  loggedIn = true
+  changeView()
+  console.log(store.user['token'])
+}
+
+const signInFailure = function (error) {
+  $('#messages').text('Failed to Sign In!')
+  $('#sign-in').trigger('reset')
+  $('#sign-up').trigger('reset')
+  console.log(error)
+}
+
+
+
+let loggedIn = false
+const changeView = function () {
+  //TODO: ADD signed in and signed out classes to other HTML ELEMENTS for clarity!!!!!!
+  if (!loggedIn) {
+    $('.signedOut').hide()
+    $('.gameBoard').hide()
+    $('#currentTurn').hide()
+  } else {
+    $('.signedOut').hide()
+
+    $('.gameBoard').show()
+    $('#currentTurn').show()
+  }
+}
+
+module.exports = {
+  signUpFailure,
+  signUpSuccess,
+  signInSuccess,
+  signInFailure
+}
