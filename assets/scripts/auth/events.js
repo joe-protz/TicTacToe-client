7d6577ec8ba46cf4e08f73988da0f7820f933905
joe10994 <joe10994@gmail.com>
2020-01-07 10:06:09 -0500

    This commit + last formatted code for readability

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -25,8 +25,6 @@ const onSignIn = function (event) {
 }
 const onSignOut = function (event) {
   event.preventDefault()
-  console.log('sign out ran')
-
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
@@ -34,8 +32,6 @@ const onSignOut = function (event) {
 
 const onChangePassword = function (event) {
   event.preventDefault()
-  console.log('change password ran!')
-
   const data = getFormFields(this)
   api.changePassword(data)
     .then(ui.changePasswordSuccess)
