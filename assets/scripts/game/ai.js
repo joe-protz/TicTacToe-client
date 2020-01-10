16276abf933925a56edb00e09147675413aaab9d
joe10994 <joe10994@gmail.com>
2020-01-09 22:04:15 -0500

    Refactor checkAiWins to also block wins
    Add param to checkAiWins to decide which player it is checking for
    win spots, so that if there is no win spot for the ai,
    it will check for the opposite player using the same logic, and
    if it finds a spot, it will block it. Created function to check
    for the other player's token.

--- assets/scripts/game/ai.js
+++ assets/scripts/game/ai.js
@@ -41,13 +41,13 @@ const aiMove = function () {
     // filter the spots left for only spots that haven't been clicked
     let currentChoice = availableSpots[Math.floor((Math.random() * availableSpots.length))]
     let aiChoice
-
+    console.log(checkAiWins(getOtherPlayer()))
   //  const availableIndexes = availableSpots.map(spot => spot.attr('id').slice(3))
-    if (checkAiWins() !== false) {
-      currentChoice = store.boxes[checkAiWins()]
-    }
-
-    //console.log(aiChoice)
+    if (checkAiWins(store.currentTurn) !== false) {
+      currentChoice = store.boxes[checkAiWins(store.currentTurn)]
+    } else if (checkAiWins(getOtherPlayer()) !== false) {
+       currentChoice = store.boxes[checkAiWins(getOtherPlayer())]
+     }
 
     currentChoice.text(store.currentTurn).addClass('clicked') // just add an x and a class clicked to the first available spot.
     const spotID = currentChoice.attr('id').slice(3) // the ID of this spot
@@ -69,58 +69,62 @@ const aiMove = function () {
 }
 
 
-const checkAiWins = function () {
-  if (store.occupiedSpots[0] === store.currentTurn && store.occupiedSpots[1] === store.currentTurn && store.occupiedSpots[2] === undefined) {
+const checkAiWins = function (turn) {
+  if (store.occupiedSpots[0] === turn && store.occupiedSpots[1] === turn && store.occupiedSpots[2] === undefined) {
     return 2
-  } else if (store.occupiedSpots[1] === store.currentTurn && store.occupiedSpots[2] === store.currentTurn && store.occupiedSpots[0] === undefined) {
+  } else if (store.occupiedSpots[1] === turn && store.occupiedSpots[2] === turn && store.occupiedSpots[0] === undefined) {
     return 0
-  } else if (store.occupiedSpots[2] === store.currentTurn && store.occupiedSpots[0] === store.currentTurn && store.occupiedSpots[1] === undefined) {
+  } else if (store.occupiedSpots[2] === turn && store.occupiedSpots[0] === turn && store.occupiedSpots[1] === undefined) {
     return 1 // all row 1
-  } else if (store.occupiedSpots[3] === store.currentTurn && store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[5] === undefined) {
+  } else if (store.occupiedSpots[3] === turn && store.occupiedSpots[4] === turn && store.occupiedSpots[5] === undefined) {
     return 5
-  } else if (store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[5] === store.currentTurn && store.occupiedSpots[3] === undefined) {
+  } else if (store.occupiedSpots[4] === turn && store.occupiedSpots[5] === turn && store.occupiedSpots[3] === undefined) {
     return 3
-  } else if (store.occupiedSpots[3] === store.currentTurn && store.occupiedSpots[5] === store.currentTurn && store.occupiedSpots[4] === undefined) {
+  } else if (store.occupiedSpots[3] === turn && store.occupiedSpots[5] === turn && store.occupiedSpots[4] === undefined) {
     return 4 // all row 2
-  } else if (store.occupiedSpots[6] === store.currentTurn && store.occupiedSpots[7] === store.currentTurn && store.occupiedSpots[8] === undefined) {
+  } else if (store.occupiedSpots[6] === turn && store.occupiedSpots[7] === turn && store.occupiedSpots[8] === undefined) {
     return 8
-  } else if (store.occupiedSpots[7] === store.currentTurn && store.occupiedSpots[8] === store.currentTurn && store.occupiedSpots[6] === undefined) {
+  } else if (store.occupiedSpots[7] === turn && store.occupiedSpots[8] === turn && store.occupiedSpots[6] === undefined) {
     return 6
-  } else if (store.occupiedSpots[6] === store.currentTurn && store.occupiedSpots[8] === store.currentTurn && store.occupiedSpots[7] === undefined) {
+  } else if (store.occupiedSpots[6] === turn && store.occupiedSpots[8] === turn && store.occupiedSpots[7] === undefined) {
     return 7 // all row 3
-  } else if (store.occupiedSpots[0] === store.currentTurn && store.occupiedSpots[3] === store.currentTurn && store.occupiedSpots[6] === undefined) {
+  } else if (store.occupiedSpots[0] === turn && store.occupiedSpots[3] === turn && store.occupiedSpots[6] === undefined) {
     return 6
-  } else if (store.occupiedSpots[3] === store.currentTurn && store.occupiedSpots[6] === store.currentTurn && store.occupiedSpots[0] === undefined) {
+  } else if (store.occupiedSpots[3] === turn && store.occupiedSpots[6] === turn && store.occupiedSpots[0] === undefined) {
     return 0
-  } else if (store.occupiedSpots[6] === store.currentTurn && store.occupiedSpots[0] === store.currentTurn && store.occupiedSpots[3] === undefined) {
+  } else if (store.occupiedSpots[6] === turn && store.occupiedSpots[0] === turn && store.occupiedSpots[3] === undefined) {
     return 3 // all col 1
-  } else if (store.occupiedSpots[1] === store.currentTurn && store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[7] === undefined) {
+  } else if (store.occupiedSpots[1] === turn && store.occupiedSpots[4] === turn && store.occupiedSpots[7] === undefined) {
     return 7
-  } else if (store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[7] === store.currentTurn && store.occupiedSpots[1] === undefined) {
+  } else if (store.occupiedSpots[4] === turn && store.occupiedSpots[7] === turn && store.occupiedSpots[1] === undefined) {
     return 1
-  } else if (store.occupiedSpots[7] === store.currentTurn && store.occupiedSpots[1] === store.currentTurn && store.occupiedSpots[4] === undefined) {
+  } else if (store.occupiedSpots[7] === turn && store.occupiedSpots[1] === turn && store.occupiedSpots[4] === undefined) {
     return 4 // all col 2
-  } else if (store.occupiedSpots[2] === store.currentTurn && store.occupiedSpots[5] === store.currentTurn && store.occupiedSpots[8] === undefined) {
+  } else if (store.occupiedSpots[2] === turn && store.occupiedSpots[5] === turn && store.occupiedSpots[8] === undefined) {
     return 8
-  } else if (store.occupiedSpots[5] === store.currentTurn && store.occupiedSpots[8] === store.currentTurn && store.occupiedSpots[2] === undefined) {
+  } else if (store.occupiedSpots[5] === turn && store.occupiedSpots[8] === turn && store.occupiedSpots[2] === undefined) {
     return 2
-  } else if (store.occupiedSpots[2] === store.currentTurn && store.occupiedSpots[8] === store.currentTurn && store.occupiedSpots[5] === undefined) {
+  } else if (store.occupiedSpots[2] === turn && store.occupiedSpots[8] === turn && store.occupiedSpots[5] === undefined) {
     return 5 /// all col 3
-  } else if (store.occupiedSpots[0] === store.currentTurn && store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[8] === undefined) {
+  } else if (store.occupiedSpots[0] === turn && store.occupiedSpots[4] === turn && store.occupiedSpots[8] === undefined) {
     return 8
-  } else if (store.occupiedSpots[0] === store.currentTurn && store.occupiedSpots[8] === store.currentTurn && store.occupiedSpots[4] === undefined) {
+  } else if (store.occupiedSpots[0] === turn && store.occupiedSpots[8] === turn && store.occupiedSpots[4] === undefined) {
     return 4
-  } else if (store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[8] === store.currentTurn && store.occupiedSpots[0] === undefined) {
+  } else if (store.occupiedSpots[4] === turn && store.occupiedSpots[8] === turn && store.occupiedSpots[0] === undefined) {
     return 0 // all diag 1
-  } else if (store.occupiedSpots[2] === store.currentTurn && store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[6] === undefined) {
+  } else if (store.occupiedSpots[2] === turn && store.occupiedSpots[4] === turn && store.occupiedSpots[6] === undefined) {
     return 6
-  } else if (store.occupiedSpots[4] === store.currentTurn && store.occupiedSpots[6] === store.currentTurn && store.occupiedSpots[2] === undefined) {
+  } else if (store.occupiedSpots[4] === turn && store.occupiedSpots[6] === turn && store.occupiedSpots[2] === undefined) {
     return 2
-  } else if (store.occupiedSpots[2] === store.currentTurn && store.occupiedSpots[6] === store.currentTurn && store.occupiedSpots[4] === undefined) {
+  } else if (store.occupiedSpots[2] === turn && store.occupiedSpots[6] === turn && store.occupiedSpots[4] === undefined) {
     return 4 // all diag 2
   } return false
 }
-
+const getOtherPlayer = function () {
+  let otherPlayer
+  store.currentTurn === 'X' ? otherPlayer = 'O' : otherPlayer = 'X'
+  return otherPlayer
+}
 module.exports = {
   takeTurn
 }
