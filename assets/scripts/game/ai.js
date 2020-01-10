7d6adad1f178f9a646ce3165223214e63669a53a
joe10994 <joe10994@gmail.com>
2020-01-10 07:51:09 -0500

    Fix AI decision making + player must wait for ai
    AI decision making updated to only specifically target corners opposite
    of opponent. Added guard to not let player play unless AI updateGame
    promise has been returned. Before, player on bad connection could
    keep playing

--- assets/scripts/game/ai.js
+++ assets/scripts/game/ai.js
@@ -2,33 +2,42 @@
 const api = require('./api')
 const ui = require('./ui')
 const store = require('../store')
+let aiTurnFinished = true
 
-const takeTurn = function (event) {
-  if (!store.gameOver) {
-    if (!($(`#${event.target.id}`).hasClass('clicked'))) { // if the spot on the board does not have the class clicked ,  add the move to the board and add the class to the spot
-      $('.warnings').text('')
-      $(`#${event.target.id}`).text(store.currentTurn).addClass('clicked')
-      store.occupiedSpots[event.target.id.slice(3)] = store.currentTurn // add the move to the store.occupiedSpots array
-      store.currentIndex = event.target.id.slice(3)
+const takeTurn = function (event) { // TODO: Add check for even clicks ('AI has taken turn')
+  if (aiTurnFinished) {
+    if (!store.gameOver) {
+      if (!($(`#${event.target.id}`).hasClass('clicked'))) { // if the spot on the board does not have the class clicked ,  add the move to the board and add the class to the spot
+        $('.warnings').text('')
+        $(`#${event.target.id}`).text(store.currentTurn).addClass('clicked')
+        store.occupiedSpots[event.target.id.slice(3)] = store.currentTurn // add the move to the store.occupiedSpots array
+        store.currentIndex = event.target.id.slice(3)
 
-      if (store.checkWin()) {
-        ui.displayWinner(store.currentTurn)
-      } else if (store.checkforTie(store.occupiedSpots)) {
-        $('#messages').text('Its a tie! Please click create game to play again')
+        if (store.checkWin()) {
+          ui.displayWinner(store.currentTurn)
+        } else if (store.checkforTie(store.occupiedSpots)) {
+          $('#messages').text('Its a tie! Please click create game to play again')
 
-        store.gameOver = true
-      }
-      api.updateGame()
-        .then(ui.updateGameSuccess)
-        .then(ui.updatePlayer)
-        .then(aiMove)
-        .catch(ui.updateGameFail)
+          store.gameOver = true
+        }
+        api.updateGame()
+          .then(ui.updateGameSuccess)
+          .then(ui.updatePlayer)
+          .then(aiTurnFinished = false)
+          .then(aiMove)
+          .catch(ui.updateGameFail)
       // ui.updatePlayer() // this updates both the variable as well as the ui
+      } else {
+        $('.warnings').text('Please click an open space')
+      }
     } else {
-      $('.warnings').text('Please click an open space')
+      $('.warnings').text('Please click Create Game to play again!')
+      setTimeout(function () {
+        $('.warnings').text('')
+      }, 2000)
     }
   } else {
-    $('.warnings').text('Please click Create Game to play again!')
+    $('.warnings').text('Please wait for AI to take turn.')
     setTimeout(function () {
       $('.warnings').text('')
     }, 2000)
@@ -39,22 +48,22 @@ const aiMove = function () {
   if (!store.gameOver) {
     const availableSpots = store.boxes.filter(box => !(box.hasClass('clicked')))
     // filter the spots left for only spots that haven't been clicked
-    let currentChoice = availableSpots[Math.floor((Math.random() * availableSpots.length))]
-    const availableIndexes = availableSpots.map(spot => spot.attr('id').slice(3))
+    let currentChoice = availableSpots[Math.floor((Math.random() * availableSpots.length))] // AI Base choice is random
+    const availableIndexes = availableSpots.map(spot => spot.attr('id').slice(3)) // index #'s for all available spots
     if (checkAiWins(store.currentTurn) !== false) { // if there is a win spot, take it
       currentChoice = store.boxes[checkAiWins(store.currentTurn)]
     } else if (checkAiWins(getOtherPlayer()) !== false) { // if there is a way to block a win, take it
       currentChoice = store.boxes[checkAiWins(getOtherPlayer())]
     } else if (availableIndexes.includes('4')) {
       currentChoice = store.boxes[4] // take center
-    } else if (availableIndexes.includes('0')) {
+    } else if (availableIndexes.includes('0') && store.occupiedSpots[8] === getOtherPlayer()) {
       currentChoice = store.boxes[0]
-    } else if (availableIndexes.includes('2')) {
+    } else if (availableIndexes.includes('2') && store.occupiedSpots[6] === getOtherPlayer()) {
       currentChoice = store.boxes[2]
-    } else if (availableIndexes.includes('6')) {
+    } else if (availableIndexes.includes('6') && store.occupiedSpots[2] === getOtherPlayer()) {
       currentChoice = store.boxes[6]
-    } else if (availableIndexes.includes('8')) {
-      currentChoice = store.boxes[8]
+    } else if (availableIndexes.includes('8') && store.occupiedSpots[0] === getOtherPlayer()) {
+      currentChoice = store.boxes[8] // opponent took took corner, and opposite is empty, take that spot
     }
 
     currentChoice.text(store.currentTurn).addClass('clicked') // just add an x and a class clicked to the first available spot.
@@ -71,6 +80,7 @@ const aiMove = function () {
     }
     api.updateGame()
       .then(ui.updateGameSuccess)
+      .then(aiTurnFinished = true)
       .catch(ui.updateGameFail)
     ui.updatePlayer()
   }
@@ -126,12 +136,12 @@ const checkAiWins = function (turn) {
   } else if (store.occupiedSpots[2] === turn && store.occupiedSpots[6] === turn && store.occupiedSpots[4] === undefined) {
     return 4 // all diag 2
   } return false
-}
+} // check if there is a win condition available, returns false if not
 const getOtherPlayer = function () {
   let otherPlayer
   store.currentTurn === 'X' ? otherPlayer = 'O' : otherPlayer = 'X'
   return otherPlayer
-}
+} // returns opposite player
 module.exports = {
   takeTurn
 }
