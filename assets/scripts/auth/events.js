5ba50172eb13077099d637e0b37582e131910cd3
joe10994 <joe10994@gmail.com>
2020-02-12 20:12:02 -0500

    add dummy login and footer

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,82 +1,102 @@
-'use strict'
+"use strict";
 
-const getFormFields = require('../../../lib/get-form-fields')
-const api = require('./api')
-const ui = require('./ui')
-const logic = require('../game/logic')
+const getFormFields = require("../../../lib/get-form-fields");
+const api = require("./api");
+const ui = require("./ui");
+const logic = require("../game/logic");
 
-const onSignUp = function (event) {
-  event.preventDefault()
-  const data = getFormFields(event.target)
-  $('.loading').show()
-  $('#messages').text('Signing up...')
+const onSignUp = function(event) {
+  event.preventDefault();
+  const data = getFormFields(event.target);
+  $(".loading").show();
+  $("#messages").text("Signing up...");
   // sign up request
-  api.signUp(data)
+  api
+    .signUp(data)
     // if sign up successful then make sign in request
     .then(response => {
-      return api.signIn(data)
+      return api.signIn(data);
     })
     // if sign in successful then run signInSuccess
     .then(ui.signInSuccess)
     .then(response => {
-      $('form').trigger('reset')
+      $("form").trigger("reset");
     })
     .catch(ui.signUpFailure)
     .then(response => {
-      $('form').trigger('reset')
-      $('.loading').hide()
-    })
-}
+      $("form").trigger("reset");
+      $(".loading").hide();
+    });
+};
 
-const onSignIn = function (event) {
-  event.preventDefault()
-  const data = getFormFields(event.target)
-  $('.loading').show()
-  $('#messages').text('Signing in...')
-  api.signIn(data)
+const onSignIn = function(event) {
+  event.preventDefault();
+  const data = getFormFields(event.target);
+  $(".loading").show();
+  $("#messages").text("Signing in...");
+  api
+    .signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
     .then(response => {
-      $('form').trigger('reset')
-      $('.loading').hide()
-    })
-}
-const onSignOut = function (event) {
-  event.preventDefault()
-  $('.loading').show()
-  $('#messages').text('Signing out...')
-  api.signOut()
+      $("form").trigger("reset");
+      $(".loading").hide();
+    });
+};
+const onSignOut = function(event) {
+  event.preventDefault();
+  $(".loading").show();
+  $("#messages").text("Signing out...");
+  api
+    .signOut()
     .then(ui.signOutSuccess)
     .then(logic.turnAiOff)
     .catch(ui.signOutFailure)
     .then(response => {
-      $('.loading').hide()
-    })
-}
+      $(".loading").hide();
+    });
+};
 
-const onChangePassword = function (event) {
-  event.preventDefault()
-  $('.loading').show()
-  $('#messages').show().text('Attempting to change password ...')
-  const data = getFormFields(event.target)
-  api.changePassword(data)
+const onChangePassword = function(event) {
+  event.preventDefault();
+  $(".loading").show();
+  $("#messages")
+    .show()
+    .text("Attempting to change password ...");
+  const data = getFormFields(event.target);
+  api
+    .changePassword(data)
     .then(ui.changePasswordSuccess)
     .catch(ui.changePasswordFailure)
     .then(response => {
-      $('.loading').hide()
-      $('#messages').text('')
-    })
-}
-
+      $(".loading").hide();
+      $("#messages").text("");
+    });
+};
+const autoSignIn = function(event) {
+  const email = "1@1";
+  const password = "1";
+  const data = {
+    credentials: {
+      email: "1@1",
+      password: "1"
+    }
+  };
+  api
+    .signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure);
+};
 const addHandlers = () => {
-  $('#sign-up').on('submit', onSignUp)
-  $('#sign-in').on('submit', onSignIn)
-  $('#sign-out').on('click', onSignOut)
-  $('#change-password-form').on('submit', onChangePassword)
-  $('#change-password').on('click', ui.openPWChange)
-  $('#cancel').on('click', ui.closePWChange)
-}
+  $(".dummy").on("click", autoSignIn);
+  $("#sign-up").on("submit", onSignUp);
+  $("#sign-in").on("submit", onSignIn);
+  $("#sign-out").on("click", onSignOut);
+  $("#change-password-form").on("submit", onChangePassword);
+  $("#change-password").on("click", ui.openPWChange);
+  $("#cancel").on("click", ui.closePWChange);
+};
 
 module.exports = {
   addHandlers
-}
+};
--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -1,83 +1,109 @@
-'use strict'
-const store = require('../store')
-const api = require('./api')
+"use strict";
+const store = require("../store");
+const api = require("./api");
 
-const displayWinner = function (currentTurn) {
-  $('#messages').text(currentTurn + ' is the winner!! Please click Create Game to play again')
-}
+const displayWinner = function(currentTurn) {
+  $("#messages").text(
+    currentTurn + " is the winner!! Please click Create Game to play again"
+  );
+};
 
-const updatePlayer = function () {
-  store.currentTurn === 'X' ? store.currentTurn = 'O' : store.currentTurn = 'X'
-  $('#currentTurn').text((`Current player is ${store.currentTurn}`))
-}
+const updatePlayer = function() {
+  store.currentTurn === "X"
+    ? (store.currentTurn = "O")
+    : (store.currentTurn = "X");
+  $("#currentTurn").text(`Current player is ${store.currentTurn}`);
+};
 
-const resetBoard = function () { // returns board colors to normal and resets messages
-  const color = $('body').css('color')
+const resetBoard = function() {
+  // returns board colors to normal and resets messages
+  const color = $("body").css("color");
   store.boxes.forEach(box => {
-    box.text(' ')
-  })
-  $('.board').css({'color': `${color}`, 'background': ``})
-  $('.warnings, #messages').text('')
-  $('#play, #currentTurn').show()
-  $('#currentTurn').text((`Current player is ${store.currentTurn}`))
-} // removes clicked class from boxes, resets text, resets all messages
+    box.text(" ");
+  });
+  $(".board").css({ color: `${color}`, background: `` });
+  $(".warnings, #messages").text("");
+  $("#play, #currentTurn").show();
+  $("#currentTurn").text(`Current player is ${store.currentTurn}`);
+}; // removes clicked class from boxes, resets text, resets all messages
 
-const updateGameSuccess = function (response) {
-  api.getGames()
+const updateGameSuccess = function(response) {
+  api
+    .getGames()
     .then(getGamesSuccess)
-    .catch(getGamesFail)
-}
-const updateGameFail = function () {
-  $('.warnings').text('Sorry, your game file could not be updated')
-}
-const createGameFail = function () {
-  $('.warnings').text('Sorry, your game could not be created')
-  $('#messages').text('')
-}
+    .catch(getGamesFail);
+};
+const updateGameFail = function() {
+  $(".warnings").text("Sorry, your game file could not be updated");
+};
+const createGameFail = function() {
+  $(".warnings").text("Sorry, your game could not be created");
+  $("#messages").text("");
+};
 
-const getGamesSuccess = function (response) {
-  store.completedGames = response.games
-  const wins = store.completedGames.filter(game => store.checkPastWins(game)).length
-  $('#stats').text('Hello ' + store.user.email + '! You have completed ' + store.completedGames.length + ' games, and you have won ' + wins)
-}
+const getGamesSuccess = function(response) {
+  store.completedGames = response.games;
+  const wins = store.completedGames.filter(game => store.checkPastWins(game))
+    .length;
+  $("#stats").text(
+    "Hello " +
+      store.user.email +
+      "! You have completed " +
+      store.completedGames.length +
+      " games, and you have won " +
+      wins
+  );
+};
 
-const getGamesFail = function () {
-  $('.warnings').text('Sorry, your game files were not able to be loaded')
-}
+const getGamesFail = function() {
+  $(".warnings").text("Sorry, your game files were not able to be loaded");
+};
 
-const winnerView = function (condition) {
-  const winColor = '#11ed46'
-  condition.forEach(index => store.boxes[index].css('color', winColor))
-  displayWinner(store.currentTurn)
-}
+const winnerView = function(condition) {
+  const winColor = "#11ed46";
+  condition.forEach(index => store.boxes[index].css("color", winColor));
+  displayWinner(store.currentTurn);
+};
 
-const toggleDifficultyButton = function (difficulty) {
+const toggleDifficultyButton = function(difficulty) {
   switch (difficulty) {
-    case 'singlePlayer':
-      $('#change-color-single').removeClass('btn-secondary').addClass('btn-primary')
-      $('#change-color-easy, #change-color-hard').removeClass('btn-primary').addClass('btn-secondary')
-      break
-    case 'hardMode':
-      $('#change-color-hard').removeClass('btn-secondary').addClass('btn-primary')
-      $('#change-color-easy, #change-color-single').removeClass('btn-primary').addClass('btn-secondary')
-      break
-    case 'easyMode':
-      $('#change-color-easy').removeClass('btn-secondary').addClass('btn-primary')
-      $('#change-color-hard, #change-color-single').removeClass('btn-primary').addClass('btn-secondary')
+    case "singlePlayer":
+      $("#change-color-single")
+        .removeClass("btn-secondary")
+        .addClass("btn-primary");
+      $("#change-color-easy, #change-color-hard")
+        .removeClass("btn-primary")
+        .addClass("btn-secondary");
+      break;
+    case "hardMode":
+      $("#change-color-hard")
+        .removeClass("btn-secondary")
+        .addClass("btn-primary");
+      $("#change-color-easy, #change-color-single")
+        .removeClass("btn-primary")
+        .addClass("btn-secondary");
+      break;
+    case "easyMode":
+      $("#change-color-easy")
+        .removeClass("btn-secondary")
+        .addClass("btn-primary");
+      $("#change-color-hard, #change-color-single")
+        .removeClass("btn-primary")
+        .addClass("btn-secondary");
   }
-}
+};
 
-const showTieView = function () {
-  $('#messages').text('Its a tie! Please click create game to play again')
-  $('.board').css('background', '#D6D8D9')
-}
+const showTieView = function() {
+  $("#messages").text("Its a tie! Please click create game to play again");
+  $(".board").css("background", "#D6D8D9");
+};
 
-const waitForTurn = function () {
-  $('.warnings').text('Please wait for AI to finish turn.')
-  setTimeout(function () {
-    $('.warnings').text('')
-  }, 2000)
-}
+const waitForTurn = function() {
+  $(".warnings").text("Please wait for AI to finish turn.");
+  setTimeout(function() {
+    $(".warnings").text("");
+  }, 2000);
+};
 module.exports = {
   displayWinner,
   updatePlayer,
@@ -91,4 +117,4 @@ module.exports = {
   toggleDifficultyButton,
   showTieView,
   waitForTurn
-}
+};
